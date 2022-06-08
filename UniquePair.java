47733dec32d8e4fa6d6578f72b24d20f43799be1
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-08 23:55:47 +0530

    Create UniquePair.java

---
+++ UniquePair.java
@@ -0,0 +1,36 @@
+package vyas;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UniquePair {
+    public static void main(String[] args) {
+        int[] arr = {1,3,7,8,5,1};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number whose unique pair needed to be displayed");
+        int num = sc.nextInt();
+        Map<Integer,Integer> unique = new HashMap<>();
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int sum = 0;
+
+            for(int j=i+1;i<arr.length;j++)
+            {
+               sum = arr[i]+arr[j];
+               if(sum ==num)
+               {
+                   int key = Math.min(arr[i],arr[j]);
+                   int value = Math.max(arr[i],arr[j]);
+                   unique.put(key,value);
+               }
+               else{
+                   continue;
+               }
+            }
+        }
+        System.out.println(unique);
+    }
+}
