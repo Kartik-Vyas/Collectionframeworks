790948187202b81bf2120becdc1f3fb351ff4bdd
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-09 00:05:32 +0530

    Update UniquePair.java
    output:-
    
    Enter the number whose unique pair needed to be displayed
    9
    {1=8, 4=5}

--- UniquePair.java
+++ UniquePair.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class UniquePair {
     public static void main(String[] args) {
-        int[] arr = {1,3,7,8,5,1};
+        int[] arr = {1,3,7,8,5,1,4};
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number whose unique pair needed to be displayed");
         int num = sc.nextInt();
@@ -17,7 +17,7 @@ public class UniquePair {
         {
             int sum = 0;
 
-            for(int j=i+1;i<arr.length;j++)
+            for(int j=i+1;j<arr.length;j++)
             {
                sum = arr[i]+arr[j];
                if(sum ==num)
