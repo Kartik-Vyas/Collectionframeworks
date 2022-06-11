f027a0619f7d9e6542caeffa924754f7afe418a3
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-12 00:27:42 +0530

    Update LL.java
    output:-
    
    is -> a -> null
    is -> a -> list -> null
    this -> is -> a -> list -> null

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -21,16 +21,49 @@ Node head;
         newNode.next=head;
         head=newNode;
     }
-    
+
     //add-last
     public void addLast(String data)
     {
-        
+        Node newNode = new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            return;
+        }
+        Node currNode=head;
+        while (currNode.next!=null)
+        {
+            currNode=currNode.next;
+        }
+        currNode.next=newNode;
+    }
+
+    //printlist
+    public void printList()
+    {
+        if(head==null)
+        {
+            System.out.println("LinkedList is empty");
+        }
+        Node currNode=head;
+        while (currNode !=null)
+        {
+            System.out.print(currNode.data + " -> ");
+            currNode=currNode.next;
+        }
+        System.out.println("null");
     }
     public static void main(String[] args) {
         LL list = new LL();
         list.addFirst("a");
         list.addFirst("is");
-        
+        list.printList();
+
+        list.addLast("list");
+        list.printList();
+
+        list.addFirst("this");
+        list.printList();
     }
 }
